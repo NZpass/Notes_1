8f29c3656c6e440f4a0d2cb427dddba05baf0cb8
NZpass <snake_28@mail.ru>
2024-02-16 14:32:41 +0300

    начало

--- notes.py
+++ notes.py
@@ -2,3 +2,18 @@
 # Программа должна уметь создавать заметку, сохранять её, читать список заметок, редактировать заметку, удалять заметку.
 # Заметка должна содержать идентификатор, заголовок, тело заметки и дату/время создания или последнего изменения заметки.
 
+import json
+import os
+from datetime import datetime
+
+# Функция для создания новой заметки
+def create_note():
+    notes = load_notes()
+    new_note = {}
+    new_note["id"] = len(notes) + 1
+    new_note["title"] = input("Введите заголовок заметки: ")
+    new_note["body"] = input("Введите текст заметки: ")
+    new_note["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    notes.append(new_note)
+    save_notes(notes)
+    print("Заметка успешно создана!")
